a49adfb4eec6a81fc30c06bfdc73ee314689b40a
xydone <xydone@outlook.com>
2025-10-03 12:25:09 +0300

    chore (crosshair): new different default crosshair

--- scripts/crosshair.lua
+++ scripts/crosshair.lua
@@ -1,12 +1,13 @@
-local size = 0.01
-
-local color = { 1,1,1,1 }
+local size = 0.01      
+local gap = 0.004       
+local color = {1,1,1,1}
 
 local crosshair_vertices = {
-    {-size, 0, color},
-    {size, 0, color},
-    { 0, size, color},
-    { 0, -size, color}
+    {-size, 0, color}, {-gap, 0, color},
+    {gap, 0, color}, {size, 0, color},
+
+    {0, -size, color}, {0, -gap, color},
+    {0, gap, color}, {0, size, color},
 }
 
 Crosshair.set_crosshair(crosshair_vertices)
